1fdc1c24812bf63182bddecaa1f4bbf232072691
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-28 10:48:39 +0100

    create a overlay test, in html create a div, in div add img, div#overlay and button#esc, in css add #overlay #overlay.active with his comand and then create comand for #esc and #esc.active, in js get node test overlay and esc and crate two addeventlistener for testing overlay

--- js/script.js
+++ js/script.js
@@ -10,15 +10,19 @@ function createCard(data) {
     pinEl.src = './img/pin.svg'
     pinEl.alt = 'puntina'
 
+    // const overlayEl = document.createElement('div')
+    // overlayEl.classList.add('overlay')
+
+    
     const imgEl = document.createElement('img')
     imgEl.classList.add('card-img')
     imgEl.src = thispolaroid.url
     imgEl.alt = thispolaroid.title
-
+    
     const dateEl = document.createElement('p');
     dateEl.textContent = thispolaroid.date;
     dateEl.classList.add('font-type');
-
+    
     const titleEl = document.createElement('h2');
     titleEl.textContent = thispolaroid.title;
     
@@ -29,11 +33,23 @@ function createCard(data) {
 
 const rowCardsEl = document.getElementById('row-cards')
 
-
-
 fetch('https://lanciweb.github.io/demo/api/pictures/')
 .then(response => response.json())
 .then(data => {
     createCard(data)
 })
 
+const overlayEl = document.getElementById('overlay')
+const imgField = document.getElementById('test')
+const escEl = document.getElementById('esc')
+imgField.addEventListener('click', () => {
+    overlayEl.classList.add('active')
+    imgField.src = './img/pin.svg'
+    escEl.classList.add('active')
+})
+
+escEl.addEventListener('click', () => {
+    escEl.classList.remove('active')
+    overlayEl.classList.remove('active')
+    imgField.src = './img/test.png'
+})
